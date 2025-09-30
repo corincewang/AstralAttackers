7140bb70a916af46a826d85b2eb418e86122b01f
corinceSub <hanwang3@andrew.cmu.edu>
2025-09-30 14:45:40 -0400

    fix: auto damage bullet after certain time

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -3,21 +3,23 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public float speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float lifeTime; 
+    
     void Start()
     {
-       
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.linearVelocity = (Vector3.up * speed);
+        
+        Destroy(this.gameObject, lifeTime);
     }
     
     void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "EnemyBomb")
         {
-            // Bullet hits bomb - both destroy each other
             Destroy(collision.gameObject); 
             Destroy(this.gameObject); 
         }
+    
     }
 }
