f5f476474df06f4954f694e25695b42750791647
corinceSub <hanwang3@andrew.cmu.edu>
2025-10-06 18:39:06 -0400

    feat: add credits page and quit button

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -19,8 +19,22 @@ public class MenuScript : MonoBehaviour
         SceneManager.LoadScene("TutorialScene");
     }
     
+    public void btn_GoToCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+    
     public void btn_GoBackToMenu()
     {
         SceneManager.LoadScene("MenuScene");
+    }
+    
+    public void btn_QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
